d7f2601390ac6653f8671e6f50c0858b951729bf
Rafael Serna <rafaelserna190@gmail.com>
2023-08-31 02:06:50 -0600

    Avance 1 - algoritmo funcional SIN dataGridView

---
+++ MonteCarlo/Algoritmos/SimulacionMonteCarlo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MonteCarlo.Algoritmos;
+
+namespace MonteCarlo.Algoritmos
+{
+    public class SimulacionMonteCarlo
+    {
+    public (double avg, double var) Algoritmo(int rows,  int cols, double lim_inf, double lim_sup)
+        {
+            Random random = new Random(); // se crea la lista con nums random
+
+            List <double> randomNumbersList = new List<double>(); // Lista en donde se guardaran los numeros
+            for (int i = 0; i < cols; i++)
+            {
+                double randomValue = random.NextDouble() * (lim_sup - lim_inf) + lim_inf; // generar nums que esten dentro del rango
+                randomNumbersList.Add(randomValue);
+            }
+
+            List<int> selectedColumnsList = new List<int>(); // se crea la lista con los num random de cols
+            for (int i = 0; i < rows; i++)
+            {
+                int colSelec = random.Next(1, cols + 1); // el num random debe esatr entre 1 y la ultima columna (incluyendola)
+                selectedColumnsList.Add(colSelec);
+            }
+
+            double sumaCols = 0; // se inicializa la variable en donde se guardara la suma
+            double colSelecCuadrado = 0; // se inicializa la variable para calcular la varianza
+            for(int i = 0; i < rows; i++)
+            {
+                int colSelec = selectedColumnsList[i] -1 ;
+                sumaCols += randomNumbersList[colSelec];
+                colSelecCuadrado += Math.Pow(randomNumbersList[colSelec], 2);
+            }
+
+            double avg = sumaCols / rows;
+            double var = (colSelecCuadrado / rows-1) - Math.Pow(avg, 2);
+
+
+            return (avg, var);
+        }
+    }
+}
--- MonteCarlo/Form1.cs
+++ MonteCarlo/Form1.cs
@@ -1,3 +1,5 @@
+using MonteCarlo.Algoritmos;
+
 namespace MonteCarlo
 {
     public partial class Form1 : Form
@@ -7,9 +9,55 @@ namespace MonteCarlo
             InitializeComponent();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text.Equals("") ||
+                textBox2.Text.Equals("") ||
+                textBox3.Text.Equals("") ||
+                textBox4.Text.Equals(""))
+            {
+                MessageBox.Show("Los numeros ingresados deben ser mayores a cero, NO pueden ser espacios en blanco.");
+                return;
+            }
+
+            // Inicialización de parámetros
+
+            int rows = Convert.ToInt32(textBox1.Text);
+            int cols = Convert.ToInt32(textBox2.Text);
+            double lim_inf = Convert.ToDouble(textBox3.Text);
+            double lim_sup = Convert.ToDouble(textBox4.Text);
+
+            SimulacionMonteCarlo simulacion = new SimulacionMonteCarlo(); // se crea una instancia de la clase
+            (double avg, double var) = simulacion.Algoritmo(rows, cols, lim_inf, lim_sup);
+
+            MessageBox.Show($"Promedio: {avg}\nVarianza: {var}");
+
+        }
+
         private void label1_Click(object sender, EventArgs e)
         {
-            // Esto es algo que comenté
+
         }
 
         private void label3_Click(object sender, EventArgs e)
